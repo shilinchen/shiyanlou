396173735623a76da0ee8b169252e5eb19b3d971
shilinchen <free1220@qq.com>
2020-07-14 11:32:29 +0800

    flaskr

--- shiyanlou/simpleFlask/flaskr/flaskr.py
+++ shiyanlou/simpleFlask/flaskr/flaskr.py
@@ -4,12 +4,19 @@ from flask import (Flask, render_template, g, flash, request, session, abort,
         redirect, url_for)
 
 # 配置项
-DATABASE = '/tmp/flaskr.db'
+DATABASE = './flaskr.db'
 ENV = 'development'
 DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
 PASSWORD = 'default'
+
+# 创建应用
+app = Flask(__name__)
+# app.config 的 from_object 方法通常用于获取对象的属性以增加配置项
+# 此处使用的参数为 __name__ ，也就是当前所在文件
+# 结果就是读取当前所在文件中的所有变量，选择其中全大写的变量作为配置项
+app.config.from_object(__name__)
 
 # 此函数的返回值是 sqlite3.connect 方法的调用，也就是连接对象
 # 这里之所以写一个函数，是因为后面的代码会多次用到连接对象
@@ -18,6 +25,83 @@ def db_conn():
     '''
 
     return sqlite3.connect(app.config['DATABASE'])
+
+# 这个装饰器的功能是，当任意视图函数执行时，预先执行这个装饰器下的所有函数
+@app.before_request
+def before():
+    '''创建数据库的连接对象，并将其赋值给 g 的 conn 属性
+    '''
+
+    # Flask 应用中有两种上下文对象：应用上下文对象和请求上下文对象
+    # g 是一个应用上下文对象，它的生存周期却是一次请求的收发
+    # 也就是说，应用每收到一次请求就会生成一个 g 对象
+    # 在生存周期内，它可以在任意视图函数中被使用
+    g.conn = db_conn()
+
+
+# 与 app.before_request 相对的是 app.after_request
+# 后者在任意视图函数执行完毕之后执行，除非视图函数执行时遇到异常
+# 而 app.teardown_request 装饰器与 app.after_request 作用一样
+# 但它无视视图函数触发的任何异常，保证一定被执行
+# 其中的参数为可能出现的异常
+@app.teardown_request
+def teardown(exception):
+    '''关闭与数据库的连接
+    '''
+
+    g.conn.close()
+
+@app.route('/')
+def show_entries():
+    '''显示所有存储在数据表中的条目
+    '''
+
+    # 获取连接对象执行查询操作之后的光标对象，该对象的 fetchall 方法中存储了查询结果
+    cursor = g.conn.execute('SELECT title, text FROM entries ORDER BY id DESC')
+    # 查询结果是一个列表，列表里是元组，将元组转换成字典
+    entries = [dict(title=row[0], text=row[1]) for row in cursor.fetchall()]
+    return render_template('show_entries.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    '''添加一条博客
+    '''
+
+    if not session.get('login'):
+        abort(401)
+    g.conn.execute('INSERT INTO entries (title, text) VALUES (?, ?)',
+            [request.form.get('title'), request.form.get('text')])
+    g.conn.commit()
+    flash('New entry has beensuccessfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    '''用户登录
+    '''
+
+    error = None
+    if request.method == 'POST':
+        # 如果用户名与配置项不符
+        if request.form.get('username') != app.config.get('USERNAME'):
+            error = 'Invalid username'
+        # 如果密码与配置项不符
+        elif request.form.get('password') != app.config.get('PASSWORD'):
+            error = 'Invalid password'
+        else:
+            session['login'] = True
+            flash('You\'re loginned successfully!')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    '''用户登出
+    '''
+
+    session.pop('login', None)
+    flash('You have logouted successfully')
+    return redirect(url_for('show_entries'))
 
 def init_db():
     '''此函数用于创建数据表，需要在 flask shell 里引入执行
@@ -44,9 +128,3 @@ def init_db():
         # 因为有些语句执行后不会立即改动数据库
         #conn.commit()
 
-# 创建应用
-app = Flask(__name__)
-# app.config 的 from_object 方法通常用于获取对象的属性以增加配置项
-# 此处使用的参数为 __name__ ，也就是当前所在文件
-# 结果就是读取当前所在文件中的所有变量，选择其中全大写的变量作为配置项
-app.config.from_object(__name__)
