d6361c2cc9a5b7a8a1954afc17cbed306c96b40e
shilinchen <free1220@qq.com>
2020-07-19 15:39:43 +0800

    uncomplete

--- shiyanlou/webFramework/main.py
+++ shiyanlou/webFramework/main.py
@@ -1,4 +1,14 @@
 from sylfk import SYLFk
 
 app = SYLFk()
+
+@app.route('/index', methods=['GET'])
+def index():
+    return '这是一个路由测试页面'
+
+
+@app.route("/test/js")
+def test_js():
+   return '<script src="/static/test.js"></script>'
+
 app.run()
